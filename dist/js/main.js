ef0cf341054707d9d00ed161822aa4e9c147457a
brae <brae@braebie.com>
2020-01-05 00:41:58 -0800

    reorganized mupsyc comps

--- dist/js/main.js
+++ dist/js/main.js
@@ -87,7 +87,7 @@ function createMuPsycContent(compLength, chordProg, numMP3s, instrument, key) {
     for(var i=1; i<=numMP3s; i++) {
         $("#"+compLength+"bars_"+chordProg+"_"+key)
         .append('<div class="col-md-6"><audio controls style="width:100%">' + 
-        '<source src="mupsyc/mp3/' + compLength + 'bars/' + key + "/" +  instrument + '/' + chordProg + '/' + chordProg + ' (' + i + ').mp3" ' +
+        '<source src="mupsyc/' + compLength + 'bars/' + key + "/" +  instrument + '/' + chordProg + '/comp (' + i + ').mp3" ' +
         'type="audio/mp3"> Your browser does not support the audio tag.' +
         '</audio></div>'
         );
