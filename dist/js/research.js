38af0cc9b8563c3bd6281ac43055a204b1fcc85e
brae <brae@braebie.com>
2020-05-30 23:37:35 -0700

    + buttons to toggle between piano & harpsichord mp3s

--- dist/js/research.js
+++ dist/js/research.js
@@ -1,31 +1,27 @@
 var g_InstrumentType; // defaults to piano
+var g_MuPsycData;
 
 $(document).ready(function(){
+    $('#pianobtn')[0].addEventListener("click", function() {
+        g_InstrumentType = 'piano'; 
+        CreateMuPsycContent();
+        document.getElementById('harpsichordbtn').classList.remove('active');
+        document.getElementById('pianobtn').classList.add('active');
+    });
+    $('#harpsichordbtn')[0].addEventListener("click", function() {
+        g_InstrumentType = 'harpsichord'; 
+        CreateMuPsycContent();
+        document.getElementById('harpsichordbtn').classList.add('active');
+        document.getElementById('pianobtn').classList.remove('active');
+    });
+
     $.getJSON("data/mupsyc-comps.json", function(muPsycData) {
         console.log("MuPsyc JSON Load Success");
 
+        g_MuPsycData = muPsycData;
         g_InstrumentType = 'piano';
 
-        var innerHTML = "<center><h3>&mu;Psyc Compositions</h3></center>";
-        var maxColumnsPerRow = 2;
-        var bootstrapWidth = 12 / maxColumnsPerRow;
-        for (i in muPsycData) {
-            innerHTML += '<center><h5>' + muPsycData[i]['bar-length'] + ' bar compositions in ' +
-            muPsycData[i]['key'] + ' with ' + muPsycData[i]['chord-prog'] + ' chord progression</h5></center>' +
-            '<div class="row row-no-gutters">';
-
-            var mp3data = muPsycData[i][g_InstrumentType + '-mp3s'];
-            for (var j = 0; j < muPsycData[i]["shown-comps"]; j++)
-            {
-                innerHTML += '<div class="col-md-' + bootstrapWidth + '"><audio controls style="width:100%">' +
-                '<source src="' + mp3data[j] + '" type="audio/mp3"> Your browser does not support the audio tag.' + 
-                '</audio></div>';
-            }
-
-            innerHTML += '</div>';
-        }
-
-        $('#mupsyc-content').html(innerHTML);
+        CreateMuPsycContent();
     })
 
     $.getJSON("data/publications.json", function(publicationData) {
@@ -44,3 +40,26 @@ $(document).ready(function(){
         $('#publication-links').html(innerHTML);
     })
 })
+
+function CreateMuPsycContent() {
+    var innerHTML = "";
+    var maxColumnsPerRow = 2;
+    var bootstrapWidth = 12 / maxColumnsPerRow;
+    for (i in g_MuPsycData) {
+        innerHTML += '<center><h5>' + g_MuPsycData[i]['bar-length'] + ' bar compositions in ' +
+        g_MuPsycData[i]['key'] + ' with ' + g_MuPsycData[i]['chord-prog'] + ' chord progression</h5></center>' +
+        '<div class="row row-no-gutters">';
+
+        var mp3data = g_MuPsycData[i][g_InstrumentType + '-mp3s'];
+        for (var j = 0; j < g_MuPsycData[i]["shown-comps"]; j++)
+        {
+            innerHTML += '<div class="col-md-' + bootstrapWidth + '"><audio controls style="width:100%">' +
+            '<source src="' + mp3data[j] + '" type="audio/mp3"> Your browser does not support the audio tag.' + 
+            '</audio></div>';
+        }
+
+        innerHTML += '</div>';
+    }
+
+    $('#mupsyc-content').html(innerHTML);
+};
