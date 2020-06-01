64b724e6cc25e04f0bc9f0b25dccce919bb40f80
brae <brae@braebie.com>
2020-05-31 17:15:52 -0700

    wait till html load is done before loading header/footer

--- dist/js/navi.js
+++ dist/js/navi.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-    $('#navi').load("navi.html");
-
-    var currentPage = window.location.pathname.split("/").pop();
-
-    $.getJSON("data/navi.json", function(navbarData) {
-        var innerHTML = '';
-
-        for (i in navbarData) {
-            innerHTML += '<li class="nav-item' + (currentPage == navbarData[i].link ? ' active' : '') + 
-            '" id="' + navbarData[i].title.toLowerCase() + '">' +
-            '<a class="nav-link" href="' + navbarData[i].link + '"><div class="nav-link-text">' +
-            navbarData[i].title + '</div></a></li>';
-        }
-
-        $('#navbar-list').html(innerHTML);
-    })
+    $('#navi').load("navi.html", function() {
+        var currentPage = window.location.pathname.split("/").pop();
+        $.getJSON("data/navi.json", function(navbarData) {
+            var innerHTML = '';
+    
+            for (i in navbarData) {
+                innerHTML += '<li class="nav-item' + (currentPage == navbarData[i].link ? ' active' : '') + 
+                '" id="' + navbarData[i].title.toLowerCase() + '">' +
+                '<a class="nav-link" href="' + navbarData[i].link + '"><div class="nav-link-text">' +
+                navbarData[i].title + '</div></a></li>';
+            }
+    
+            $('#navbar-list').html(innerHTML);
+        })
+    });
 })
